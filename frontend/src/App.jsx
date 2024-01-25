16d60fca131f8f3e54d59b01b1f30fe4370fcec6
ghollamsimo <ghollamsimo1@gmail.com>
2024-01-25 21:15:15 +0100

    Ading In Folder FrontEnd A Login Page And Folder Hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { Fragment, useState } from 'react'
+import React ,{ Fragment} from 'react'
 import Navbar from './components/nav/Navbar'
+import Footer from "./components/footer/Footer.jsx";
 import './App.css'
 
 function App() {
 
   return (
     <Fragment>
-      <Navbar />
+        <Navbar />
+
+        <Footer/>
     </Fragment>
   )
 }
