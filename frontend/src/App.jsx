22447db53b8f5c467b1054570f4450dc4cd722f6
ghollamsimo <ghollamsimo1@gmail.com>
2024-01-25 14:00:02 +0100

    Add Folder FrontEnd And FolderBackend And Implement React And Laravel

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment, useState } from 'react'
+import Navbar from './components/nav/Navbar'
+import './App.css'
+
+function App() {
+
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  )
+}
+
+export default App
